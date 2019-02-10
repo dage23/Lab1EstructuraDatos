8c208b5156de0893c7269c5d0d84c698c9bdf868
dage23 <46784507+dage23@users.noreply.github.com>
2019-02-10 12:38:18 -0600

    Agregada interfaz de Estructuras Lineales

---
+++ EstructurasLineales/Nodo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstructurasLineales
+{
+    public class Nodo<T>
+    {
+        public T Informacion { get; set; }
+        public Nodo<T> nSiguiente{ get; set; }
+        public Nodo<T> nAnterior { get; set; }
+
+        public Nodo(T value)
+        {
+            Informacion = Informacion;
+            nSiguiente = null;
+            nAnterior = null;
+        }
+    }
+}
---
+++ FarmaciaLab1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FarmaciaLab1.MvcApplication" Language="C#" %>
